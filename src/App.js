675b8d70d3021d7a507fe3a9eadf8d57ef159980
Luis Lozano <luislozano@MacBook-Air.local>
2020-03-24 13:18:38 -0600

    Add components, data, images and pages. Established routes and NavBar functionality.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-//import './App.css';
+import './App.css';
+
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+import Error from './pages/Error'
+import Navbar from './components/Navbar'
+
+import { Route, Switch } from 'react-router-dom'
+
 
 function App() {
   return (
     <>
-    Hello from app!
-    </>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/rooms" component={Rooms}/>
+        <Route exact path="/rooms/:id" component={SingleRoom}/>
+        <Route component={Error}/>
+      </Switch>
+    </> 
   );
 }
 
