e57d2e95ef62bc90f7105767d4b93de5a4afcfde
Luis Lozano <luislozano@MacBook-Air.local>
2020-04-03 17:17:43 -0600

    Added functionality to search for room and number of guests

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -3,7 +3,13 @@ import { useContext } from 'react'
 import { RoomContext } from '../context'
 import Title from '../components/Title'
 
-function RoomFilter() {
+// get all unique values
+
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))]
+} 
+
+function RoomFilter({rooms}) {
 
   const context = useContext(RoomContext)
   const {
@@ -19,9 +25,64 @@ function RoomFilter() {
     pets 
   } = context
 
+  // get unique types
+  let types = getUnique(rooms, 'type')
+
+  // add all
+  types = ['all', ...types]
+
+  // map to jsx
+  types = types.map((item, index) => {
+    return (
+      <option key={index} value={item}>
+      {item}
+      </option>
+    )
+  })
+
+  let people = getUnique(rooms, 'capacity')
+
+  people = people.map((item, index) => {
+    return (
+      <option key={index} value={item}>
+        {item}
+      </option>
+    )
+  })
+
   return (
     <section className="filter-container">
       <Title title="search rooms" />
+      <form className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select 
+            name="type" 
+            id="type" 
+            value={type} 
+            className="form-control" 
+            onChange={handleChange}
+            >
+            {types}
+          </select>
+        </div>
+        {/* end of select type */}
+        {/* guests */}
+        <div className="form-group">
+          <label htmlFor="capacity">Guests</label>
+          <select 
+            name="capacity" 
+            id="capacity" 
+            value={capacity} 
+            className="form-control" 
+            onChange={handleChange}
+            >
+            {people}
+          </select>
+        </div>
+        {/* end of guests */}
+      </form>
     </section>
   )
 }
