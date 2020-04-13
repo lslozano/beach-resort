a91cb2476739dc70bc15b73f5bddabd307b4487a
Luis Lozano <lslozano11@gmail.com>
2020-04-13 15:59:18 -0500

    Finished project.

--- src/context.js
+++ src/context.js
@@ -2,13 +2,7 @@ import React, { Component } from 'react';
 import items from './data';
 
 const RoomContext = React.createContext();
-/*
-First one is a provider (RoomContext) 
-and second one is a consumer.
 
-Consumer:
-<RoomContext.Provider value={'hello'}
-*/
 class RoomProvider extends Component {
 
   state = {
@@ -46,7 +40,7 @@ class RoomProvider extends Component {
 
   handleChange = event => {
     const target = event.target
-    const value = event.type === 'checkbox' ? target.checked : target.value
+    const value = target.type === 'checkbox' ? target.checked : target.value
     const name = event.target.name
     this.setState({
       [name]: value,
@@ -71,6 +65,7 @@ class RoomProvider extends Component {
   
     // transform values
     capacity = parseInt(capacity)
+    price = parseInt(price)
 
     // filter by type
     if (type !== 'all') {
@@ -82,13 +77,30 @@ class RoomProvider extends Component {
       tempRooms = tempRooms.filter(room => room.capacity >= capacity)
     }
 
+    // filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price)
+
+    // filter by size
+    tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+
+    // filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true)
+    }
+
+    // filter by pet
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true)
+    }
+
+    // change state
     this.setState({
       sortedRooms: tempRooms
     })
-
   }
 
   componentDidMount() {
+
     // this.getData
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
